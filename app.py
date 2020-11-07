1429c0e5d4de60fcf8cf3148110be7f5128fe834
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-11-07 08:40:34 -0700

    Finished Week20B Assignment

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ conn = None
 cursor = None
 
 try:
-
     conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, port=dbcreds.port, database=dbcreds.database, host=dbcreds.host)
     cursor = conn.cursor()
     alias = input("Please type your alias: ")
@@ -14,28 +13,29 @@ try:
     cursor.execute("SELECT * FROM hackers WHERE alias=? AND password=?", [alias,password,])
     user = cursor.fetchone()
     if user:
-        print("User Login Successful!\n --------------------------")
-    while True:
-        print("Select an Option:\n 1. Create an exploit\n 2. See all of your exploits\n 3. See all other aliases exploits\n 4. Exit the application \n --------------------------")
-        user_choice = input("Enter your Option: ")
+        if password == user[2]:
+            print("User Login Successful!\n --------------------------")
+            while True:
+                print("\nSelect an Option:\n 1. Create an exploit\n 2. See all of your exploits\n 3. See all other aliases exploits\n 4. Exit the application \n --------------------------")
+                user_choice = input("\nEnter your Option: ")
        
-        if user_choice == "1":
-            functions.CreateExploit(user)
+                if user_choice == "1":
+                    functions.CreateExploit(user)
         
-        elif user_choice == "2":
-            functions.ViewYourExploits(user)
+                elif user_choice == "2":
+                    functions.ViewYourExploits(user)
         
-        elif user_choice == "3":
-            functions.ViewOthersExploits(user)
+                elif user_choice == "3":
+                    functions.ViewOthersExploits(user)
             
-        elif user_choice == "4":
-            print("--------------------------\n Good-Bye!")
-            break
+                elif user_choice == "4":
+                    print("--------------------------\nGood-Bye!")
+                    break
              
-        else: 
-            print("Please try again, invalid input!")
+                else: 
+                    print("Please try again, invalid input!")
     else: 
-        print("Try again!")
+        print("--------------------------\nTry again! Invalid user or password.")
 
 
 except mariadb.ProgrammingError:
@@ -44,6 +44,14 @@ except mariadb.ProgrammingError:
 except mariadb.OperationalError:
     print("There seems to be something wrong with the connection.")
 
+except mariadb.IntegrityError:
+    print("That would have damaged the database!")
+
+except mariadb.InternalError:
+    print("Internal error of database!")
+
+except:
+    print("An unanticipated error has occured.")
 
 finally:
     if(cursor != None):
--- functions.py
+++ functions.py
@@ -2,29 +2,75 @@ import dbcreds
 import mariadb
 
 def CreateExploit(user):
-    content = input("Write your exploit: ")
-    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO exploits (content, user_id) VALUES(?, ?)", [content, user[0],])
-    conn.commit()
-    print("Exploit added succesfully!")
-    cursor.close()
-    conn.close()
+    try:
+        content = input("Write your exploit: ")
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO exploits (content, user_id) VALUES(?, ?)", [content, user[0],])
+        conn.commit()
+        print("--------------------------\n Exploit added succesfully! \n--------------------------" )
+        cursor.close()
+        conn.close()
+    
+    except mariadb.ProgrammingError:
+        print("Programming Error!")
+
+    except mariadb.OperationalError:
+        print("There seems to be something wrong with the connection.")
+     
+    except mariadb.IntegrityError:
+        print("That would have damaged the database!")
+
+    except mariadb.InternalError:
+        print("Internal error of database!")
+
+    except:
+        print("An unanticipated error has occured.")
 
 def ViewYourExploits(user):
-    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM exploits WHERE user_id = ?", [user[0],])
-    posts = cursor.fetchall()
-    for post in posts:
-        print("exploits id: " + str((post[0])))
-        print("content: " + str((post[1])))
+    try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM exploits WHERE user_id = ?", [user[0],])
+        posts = cursor.fetchall()
+        for post in posts:
+            print("\ncontent: " + str((post[1])))
+
+    except mariadb.ProgrammingError:
+        print("Programming Error!")
+
+    except mariadb.OperationalError:
+        print("There seems to be something wrong with the connection.")
+    
+    except mariadb.IntegrityError:
+        print("That would have damaged the database!")
+
+    except mariadb.InternalError:
+        print("Internal error of database!")
+
+    except:
+        print("An unanticipated error has occured.")
 
 def ViewOthersExploits(user):
-    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM exploits WHERE user_id != ?", [user[0],])
-    posts = cursor.fetchall()
-    for post in posts:
-        print("exploits id: " + str((post[0])))
-        print("content: " + str((post[1])))
+    try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM exploits WHERE user_id != ?", [user[0],])
+        posts = cursor.fetchall()
+        for post in posts:
+            print("\ncontent: " + str((post[1])))
+
+    except mariadb.ProgrammingError:
+        print("Programming Error!")
+
+    except mariadb.OperationalError:
+        print("There seems to be something wrong with the connection.")
+        
+    except mariadb.IntegrityError:
+        print("That would have damaged the database!")
+
+    except mariadb.InternalError:
+        print("Internal error of database!")
+
+    except:
+        print("An unanticipated error has occured.")
